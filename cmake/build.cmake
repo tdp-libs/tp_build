86a5eefc2fab2fc032f89e62b62600ff5d17a6d4
Tom Paynter <tompaynter@tdpe.co.uk>
2019-08-07 21:20:28 +0100

    Started to add tests.

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -1,3 +1,7 @@
+enable_testing()
+add_custom_target(check
+        COMMAND env CTEST_OUTPUT_ON_FAILURE=1 GTEST_COLOR=1 ${CMAKE_CTEST_COMMAND})
+
 function(tdp_parse_submodules directory)
 
   execute_process(COMMAND "${CMAKE_CURRENT_LIST_DIR}/tdp_build/cmake/extract_submodules.sh" SUBDIRS
---
+++ tp_test/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+testCommands=(./test_tp_utils/test_tp_utils ./test_tp_utils/test_tp_utils ./test_tp_utils/test_tp_utils)
+
+results="\nTest Results:\n"
+for testCommand in ${testCommands[@]}; do
+  echo -e "\n\e[1;93mRunning test: \e[21m${testCommand}\e[39m"
+
+  if ${testCommand}; then
+    results="${results}    \e[32mPassed: ${testCommand}\e[39m\n"
+  else
+    results="${results}    \e[31mFailed: ${testCommand}\e[39m\n"
+  fi
+done
+
+echo -e "${results}"
+

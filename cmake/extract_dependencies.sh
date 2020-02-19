ae2a17cad55ee14efa6b1a2b61e78a5eab5c7350
Tom Paynter <tompaynter@tdpe.co.uk>
2020-02-19 16:48:31 +0000

    Added find package code for Papillon and EdgeVis.

--- cmake/extract_dependencies.sh
+++ cmake/extract_dependencies.sh
@@ -5,7 +5,7 @@ RESULT=""
 env -i make -pn -f ../tp_build/cmake/parse_dependencies.pri > $TMP_DB_FILE 2>/dev/null
 while read var assign value; do
   if [[ ${var} = $1 ]] && [[ ${assign} = '=' ]]; then
-    RESULT="$value"
+    RESULT+="$value "
     break
   fi
 done < $TMP_DB_FILE
@@ -13,7 +13,7 @@ done < $TMP_DB_FILE
 env -i make -pn -f ../project.inc > $TMP_DB_FILE 2>/dev/null
 while read var assign value; do
   if [[ ${var} = $1 ]] && [[ ${assign} = '=' ]]; then
-    RESULT+=" $value"
+    RESULT+="$value "
     break
   fi
 done < $TMP_DB_FILE

963dee282bb4e33fe8c0090fd6c204b5b76fef0c
Tom Paynter <tompaynter@tdpe.co.uk>
2020-05-13 14:40:11 +0100

    Fixing windows cmake linker issues.

--- dependencies/filesystem/cmake.cmake
+++ dependencies/filesystem/cmake.cmake
@@ -1,6 +1,9 @@
-list(APPEND TP_LIBRARIES "-lstdc++fs")
-list(APPEND TP_LIBRARIES "stdc++fs")
-list(APPEND TP_LIBRARIES "libstdc++fs")
-list(APPEND TP_LIBRARIES "-lc++fs")
-list(APPEND TP_LIBRARIES "c++fs")
-list(APPEND TP_LIBRARIES "libc++fs")
+
+if(WIN32)
+  list(APPEND TP_LIBRARIES "libstdc++fs")
+  # list(APPEND TP_LIBRARIES "c++fs")
+  # list(APPEND TP_LIBRARIES "libc++fs")
+else()
+  list(APPEND TP_LIBRARIES "-lstdc++fs")
+endif()
+

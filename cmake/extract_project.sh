03f653eebcf0b9ba9fcf7cd0129de3ab568c933e
Tom Paynter <tompaynter@tdpe.co.uk>
2021-11-24 15:29:19 +0000

    Fix to cmak build.

--- cmake/extract_project.sh
+++ cmake/extract_project.sh
@@ -2,7 +2,7 @@
 TMP_DB_FILE="/tmp/$$_make.db.txt"
 RESULT=""
 
-env -i `which make` -pn -f ../project.inc > $TMP_DB_FILE 2>/dev/null
+env -i `which make` -pn -f project.inc > $TMP_DB_FILE 2>/dev/null
 while read var assign value; do
   if [[ ${var} = $1 ]] && [[ ${assign} = '=' ]]; then
     RESULT+="$value "
